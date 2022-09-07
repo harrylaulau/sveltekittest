9fcce222c379bcfd390be5caa9d961a8b60f8b56
harrylaulau <dev+github@harrylaulau.me>
2022-09-07 09:36:16 +0100

    change README

---
+++ README.md
@@ -0,0 +1,8 @@
+created with:
+npm create svelte@latest test
+
+Options: skeleton project, typescript, eslint, prttier enabled.
+
+Edited Files:
+src/routes/+page.svelte
+src/routes/+page.server.ts
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { ActionData } from './$types';
+	export let form: ActionData;
+	$: console.log(form?.success);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form method="POST">
+	<button>Log in</button>
+</form>
